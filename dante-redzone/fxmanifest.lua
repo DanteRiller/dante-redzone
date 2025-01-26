d8de512a3f8c64d676c15a39546f00ebec2eea4c
Dante Riller <cengizomer763@gmail.com>
2025-01-27 02:06:52 +0300

    dante-redzone
    dante-redzone

---
+++ dante-redzone/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '!Dante'
+description 'RedZone Script'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+shared_scripts {
+    'config.lua'
+}
